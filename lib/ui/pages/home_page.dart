4116cefb30d99aa0060ed5d0aff09edeed6ebaca
Hanan795 <hawawdeh95@gmail.com>
2022-01-30 15:30:09 +0200

    create input field widget #10

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:to_do_app/services/theme_services.dart';
 import 'package:to_do_app/ui/pages/notification_screen.dart';
+import 'package:to_do_app/ui/size_config.dart';
 import 'package:to_do_app/ui/widgets/button.dart';
+import 'package:to_do_app/ui/widgets/input_field.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,20 +16,31 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
             onPressed: () {
               ThemeServices().swithTheme();
-              Get.to(NotificationScreen(payLoad: 'Title|Desc|10:10'));
+              Get.to(const NotificationScreen(payLoad: 'Title|Desc|10:10'));
             },
-            icon: Icon(Icons.dark_mode)),
+            icon: const Icon(Icons.dark_mode)),
       ),
-      body: Center(
-        child: MyButton(
-          label: 'Add Task',
-          onTap: () {},
-        ),
+      body: SingleChildScrollView(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              MyButton(
+                label: 'Add Task',
+                onTap: () {},
+              ),
+              const InputField(
+                title: 'title',
+                hint: 'note',
+                widget: Icon(Icons.access_alarm),
+              )
+            ]),
       ),
     );
   }
