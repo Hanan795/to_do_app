794d4355b69351f8e87206b72e71de955a875fbe
Hanan795 <hawawdeh95@gmail.com>
2022-01-30 14:22:01 +0200

    create these service to save, get, and switch themeMode of the app, and create Icon button to switch te theme whin it is click #4 #6

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:to_do_app/services/theme_services.dart';
 import 'ui/pages/home_page.dart';
 import 'ui/pages/notification_screen.dart';
 import 'ui/theme.dart';
@@ -18,8 +19,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: Themes.light,
       darkTheme: Themes.dark,
-      themeMode: ThemeMode.light,
-      home: const NotificationScreen(payLoad: 'Title|Description|Date'),
+      themeMode: ThemeServices().theme,
+      home: const HomePage(),
     );
   }
 }
--- lib/services/theme_services.dart
+++ lib/services/theme_services.dart
@@ -1,1 +1,19 @@
-class ThemeServices {}
+import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:get/get.dart';
+
+class ThemeServices {
+  final GetStorage _box = GetStorage();
+  final _key = 'isDarkMode';
+
+  ThemeMode get theme => _loadThemeFromBox() ? ThemeMode.dark : ThemeMode.light;
+
+  bool _loadThemeFromBox() => _box.read<bool>(_key) ?? false;
+
+  _saveThemeToBox(bool isDarkMode) => _box.write(_key, isDarkMode);
+
+  void swithTheme() {
+    Get.changeThemeMode(_loadThemeFromBox() ? ThemeMode.light : ThemeMode.dark);
+    _saveThemeToBox(!_loadThemeFromBox());
+  }
+}
