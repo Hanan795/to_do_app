4bc6788147ea612e023952e989e628a01e1bdf46
Hanan795 <hawawdeh95@gmail.com>
2022-02-02 13:59:10 +0200

    edit showTasks method  and add some animation to the tasks #17

--- lib/controllers/task_controller.dart
+++ lib/controllers/task_controller.dart
@@ -2,7 +2,32 @@ import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:to_do_app/models/task.dart';
 
 class TaskController extends GetxController {
-  final taskList = <Task>[];
+  final taskList = <Task>[
+    Task(
+      title: 'Title 1',
+      note: 'Note Something',
+      isCompleted: 0,
+      startTime: '12:50',
+      endTime: '16:40',
+      color: 1,
+    ),
+    Task(
+      title: 'Title 2',
+      note: 'Note Something',
+      isCompleted: 1,
+      startTime: '12:50',
+      endTime: '16:40',
+      color: 2,
+    ),
+    Task(
+      title: 'Title 3',
+      note: 'Note Something',
+      isCompleted: 0,
+      startTime: '12:50',
+      endTime: '16:40',
+      color: 3,
+    ),
+  ];
 
   getTasks() {}
 }
