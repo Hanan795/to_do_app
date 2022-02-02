21d77c382e49d08256dff9361f6e06838203019d
Hanan795 <hawawdeh95@gmail.com>
2022-02-02 15:26:47 +0200

    edit sceduledNotification method in Notification Service file #22

--- lib/controllers/task_controller.dart
+++ lib/controllers/task_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:intl/intl.dart';
 import 'package:to_do_app/models/task.dart';
 
 class TaskController extends GetxController {
@@ -7,26 +8,27 @@ class TaskController extends GetxController {
       title: 'Title 1',
       note: 'Note Something',
       isCompleted: 0,
-      startTime: '12:50',
-      endTime: '16:40',
+      startTime: DateFormat('hh:mm a')
+          .format(DateTime.now().add(const Duration(minutes: 1))),
+      // endTime: '16:40',
       color: 1,
     ),
-    Task(
-      title: 'Title 2',
-      note: 'Note Something',
-      isCompleted: 1,
-      startTime: '12:50',
-      endTime: '16:40',
-      color: 2,
-    ),
-    Task(
-      title: 'Title 3',
-      note: 'Note Something',
-      isCompleted: 0,
-      startTime: '12:50',
-      endTime: '16:40',
-      color: 3,
-    ),
+    // Task(
+    //   title: 'Title 2',
+    //   note: 'Note Something',
+    //   isCompleted: 1,
+    //   startTime: '12:50',
+    //   endTime: '16:40',
+    //   color: 2,
+    // ),
+    // Task(
+    //   title: 'Title 3',
+    //   note: 'Note Something',
+    //   isCompleted: 0,
+    //   startTime: '12:50',
+    //   endTime: '16:40',
+    //   color: 3,
+    // ),
   ];
 
   getTasks() {}
