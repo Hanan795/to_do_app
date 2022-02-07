8da8d36205f18660e8a0cb6445ad618930477534
Hanan795 <hawawdeh95@gmail.com>
2022-02-07 13:42:57 +0200

    Create database and initialize it in the main #31

--- lib/db/db_helper.dart
+++ lib/db/db_helper.dart
@@ -1,1 +1,36 @@
-class DBHelper {}
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import '../models/task.dart';
+
+class DBHelper {
+  static Database? _db;
+  static const int _version = 1;
+  static const String _tableName = 'tasks';
+
+  static Future<void> initDb() async {
+    if (_db != null) {
+      debugPrint('not null bd');
+      return;
+    } else {
+      try {
+        String _path = await getDatabasesPath() + 'task.db';
+        debugPrint('in database path');
+        _db = await openDatabase(_path, version: _version,
+            onCreate: (Database db, int version) async {
+          debugPrint('Creating a new one');
+          await db.execute(
+            'CREATE TABLE $_tableName ('
+            'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+            'title STRING, note TEXT, date STRING, '
+            'startTime STRING, endTime STRING, '
+            'remind INTEGER, repeat STRING, '
+            'color INTEGER, '
+            'isCompleted INTEGER)',
+          );
+        });
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
-import 'package:to_do_app/services/theme_services.dart';
+import 'package:get_storage/get_storage.dart';
+
+import '../db/db_helper.dart';
+import '../services/theme_services.dart';
 import 'ui/pages/home_page.dart';
 import 'ui/theme.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DBHelper.initDb();
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
